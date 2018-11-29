c92f2e13f4911af71b6a95fef12099c9a2b35611
谭立清 <liqing.tlq@alibaba-inc.com>
2018-11-29 17:31:04 +0800

    refactor(layout): sidebar init

--- app/modules/layout/base/index.tsx
+++ app/modules/layout/base/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Layout, Breadcrumb } from 'antd';
+import Sider from './sider';
 
-const { Header, Content, Sider } = Layout;
-const { SubMenu } = Menu;
+const { Header, Content } = Layout;
 
 interface IProps {
   children: string | React.ReactNode | (() => React.ReactNode)
@@ -17,57 +17,14 @@ const showContent = () => {
 };
 
 export default class AppLayout extends React.PureComponent<IProps> {
-  state = {
-    collapsed: false,
-  };
-
   componentDidMount() {
     showContent();
-  }
-
-  onCollapse = (collapsed: boolean) => {
-    this.setState({ collapsed });
   }
 
   render() {
     return (
       <Layout style={{ minHeight: '100vh' }}>
-        <Sider
-          collapsible
-          collapsed={this.state.collapsed}
-          onCollapse={this.onCollapse}
-        >
-          <div className="logo" />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1">
-              <Icon type="pie-chart" />
-              <span>Option 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="desktop" />
-              <span>Option 2</span>
-            </Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={<span><Icon type="user" /><span>User</span></span>}
-            >
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={<span><Icon type="team" /><span>Team</span></span>}
-            >
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9">
-              <Icon type="file" />
-              <span>File</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
+        <Sider />
         <Layout>
           <Header style={{ background: '#fff', padding: 0 }}>
             <Breadcrumb style={{ margin: '16px 0' }}>

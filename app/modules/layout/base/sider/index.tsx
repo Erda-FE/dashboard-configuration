8ffd1b0a5a00e5b1d5718469933666a997c64e89
谭立清 <liqing.tlq@alibaba-inc.com>
2018-12-03 11:06:19 +0800

    style(sider): add style

--- app/modules/layout/base/sider/index.tsx
+++ app/modules/layout/base/sider/index.tsx
@@ -1,55 +1,37 @@
 import React from 'react';
-import { Layout, Menu, Icon } from 'antd';
+import { Layout } from 'antd';
+import { Icon as ColorIcon } from 'common';
+import SiderMenu from './menu';
+import styles from './index.scss';
 
 const { Sider } = Layout;
-const { SubMenu } = Menu;
 
 export default class AppSidebar extends React.PureComponent {
   state = {
     collapsed: false,
   };
 
-  onCollapse = (collapsed: boolean) => {
-    this.setState({ collapsed });
+  onCollapse = () => {
+    this.setState({ collapsed: !this.state.collapsed });
   }
 
   render() {
+    const { collapsed } = this.state;
     return (
       <Sider
         collapsible
-        collapsed={this.state.collapsed}
-        onCollapse={this.onCollapse}
+        collapsed={collapsed}
+        className={styles.sider}
+        trigger={(
+          <div className={styles.trigger} onClick={this.onCollapse}>
+            <ColorIcon
+              className="trigger-icon"
+              type={collapsed ? 'open' : 'close'}
+            />
+          </div>
+        )}
       >
-        <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key="1">
-            <Icon type="pie-chart" />
-            <span>Option 1</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="desktop" />
-            <span>Option 2</span>
-          </Menu.Item>
-          <SubMenu
-            key="sub1"
-            title={<span><Icon type="user" /><span>User</span></span>}
-          >
-            <Menu.Item key="3">Tom</Menu.Item>
-            <Menu.Item key="4">Bill</Menu.Item>
-            <Menu.Item key="5">Alex</Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="sub2"
-            title={<span><Icon type="team" /><span>Team</span></span>}
-          >
-            <Menu.Item key="6">Team 1</Menu.Item>
-            <Menu.Item key="8">Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="9">
-            <Icon type="file" />
-            <span>File</span>
-          </Menu.Item>
-        </Menu>
+        <SiderMenu />
       </Sider>
     );
   }

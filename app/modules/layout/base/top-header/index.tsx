a96492ca00748ed836ebcd3baf6420de8ad50ffc
谭立清 <liqing.tlq@alibaba-inc.com>
2018-12-03 14:21:36 +0800

    feat(top-header): init

--- app/modules/layout/base/top-header/index.tsx
+++ app/modules/layout/base/top-header/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Layout, Breadcrumb } from 'antd';
+import { Link } from 'dva/router';
+import styles from './index.scss';
 
 const { Header } = Layout;
+const { Item } = Breadcrumb;
 
-const AppHeader = () => (
-  <Header style={{ background: '#fff', padding: 0 }}>
-    <Breadcrumb style={{ margin: '16px 0' }}>
-      <Breadcrumb.Item>User</Breadcrumb.Item>
-      <Breadcrumb.Item>Bill</Breadcrumb.Item>
-    </Breadcrumb>
-  </Header>
-);
+// @trick Item、Breadcrumb有未知原因的warning
+const AppHeader = () => {
+  const breadcrumbItems = [
+    <Item key="home">
+      <Link to="/">首页</Link>
+    </Item>,
+    <Item key="user">User</Item>,
+    <Item key="bill">Bill</Item>];
+  return (
+    <Header className={styles.header}>
+      <Breadcrumb className={styles.breadcrumb} separator=">">
+        {breadcrumbItems}
+      </Breadcrumb>
+    </Header>
+  );
+};
 
 export default AppHeader;

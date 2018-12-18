b8e978b91d635358f92c97eb8d3a5232b8a618c1
谭立清 <liqing.tlq@alibaba-inc.com>
2018-12-18 11:02:31 +0800

    feat(board): add back cell image

--- app/router/dashboard/board/index.tsx
+++ app/router/dashboard/board/index.tsx
@@ -12,6 +12,26 @@ import 'react-resizable/css/styles.css';
 
 type IProps = ISizeMe;
 
+const GRID_MARGIN = 10; // Cell间距
+const RECT_BORDER_WIDTH = 1; // rect border宽度
+const cols = 12;
+const rowHeight = 30;
+const getCellSize = (width: number) => ({
+  width: (width - GRID_MARGIN) / cols,
+  height: rowHeight + GRID_MARGIN,
+});
+
+const getGridBackground = (width: number) => {
+  const cellSize = getCellSize(width);
+  const front = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='${(cellSize.width * cols) + (RECT_BORDER_WIDTH * 1)}' height='${cellSize.height}'>`;
+  const back = '</svg>")';
+  let colsStr = '';
+  for (let i = 0; i < cols; i += 1) {
+    colsStr += `<rect rx='4' ry='4' style='fill: #E5E9F2; opacity: 0.5' stroke-width='${RECT_BORDER_WIDTH}' fill='none' x='${Math.round(i * cellSize.width) + GRID_MARGIN}' y='${GRID_MARGIN}' width='${Math.round(cellSize.width - GRID_MARGIN - RECT_BORDER_WIDTH)}' height='${cellSize.height - GRID_MARGIN - RECT_BORDER_WIDTH}'/>`;
+  }
+  return `${front}${colsStr}${back}`;
+};
+
 const names = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 const datas = [{
   data: [820, 932, 901, 934, 1290, 1330, 1320],
@@ -22,22 +42,25 @@ const layout = [
   { i: 'c', x: 4, y: 0, w: 1, h: 6 },
 ];
 
+
 class Board extends React.PureComponent<IProps> {
   onLayoutChange = () => {
 
   }
 
   render() {
-    const { size: { width } } = this.props;
+    const { size } = this.props;
+    const { width } = size;
     return (
       <div>
         <ReactGridLayout
           autoSize
           layout={layout}
-          cols={12}
+          cols={cols}
           rowHeight={30}
           width={width}
           onLayoutChange={this.onLayoutChange}
+          style={{ backgroundImage: getGridBackground(width) }}
         >
           <div key="a"><ChartLine names={names} datas={datas} /></div>
           <div key="b"><ChartLine names={names} datas={datas} /></div>

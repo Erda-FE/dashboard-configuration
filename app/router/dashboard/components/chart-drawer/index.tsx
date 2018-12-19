423003f34182e965cf4288d9d02efe83333bb3da
谭立清 <liqing.tlq@alibaba-inc.com>
2018-12-19 11:24:32 +0800

    style(chart-drawer): content and footer style change

--- app/router/dashboard/components/chart-drawer/index.tsx
+++ app/router/dashboard/components/chart-drawer/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Drawer } from 'antd';
+import { Drawer, Button } from 'antd';
+import './index.scss';
 
 type IProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
 
@@ -14,7 +15,19 @@ class ChartDrawer extends React.PureComponent<IProps> {
         width={500}
         onClose={closeDrawer}
         mask={false}
-      />
+        closable={false}
+        className="bi-drawer"
+      >
+        <div className="bi-drawer-content" />
+        <div className="bi-drawer-footer">
+          <Button onClick={closeDrawer} style={{ marginRight: 8 }}>
+            取消
+          </Button>
+          <Button onClick={closeDrawer} type="primary">
+            提交
+          </Button>
+        </div>
+      </Drawer>
     );
   }
 }

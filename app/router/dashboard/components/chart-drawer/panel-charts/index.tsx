78f67266aca856534408fae148eb6f48ac7dda9e
谭立清 <liqing.tlq@alibaba-inc.com>
2018-12-22 20:41:50 +0800

    feat(chart): add chartNameMap

--- app/router/dashboard/components/chart-drawer/panel-charts/index.tsx
+++ app/router/dashboard/components/chart-drawer/panel-charts/index.tsx
@@ -2,28 +2,29 @@ import React from 'react';
 import classnames from 'classnames';
 import { connect } from 'dva';
 import { Collapse, Tooltip } from 'antd';
+import { chartNameMap } from 'dashboard/utils';
 import './index.scss';
 
 const { Panel } = Collapse;
 
 const charts = [
-  { type: 'bar', name: '柱状图', img: '/images/charts/bar-heap-on.png' },
-  { type: 'line', name: '折线图', img: '/images/charts/line-on.png' },
-  { type: 'area', name: '面积图', img: '/images/charts/line-area-on.png' },
-  { type: 'pie', name: '饼图', img: '/images/charts/pie-on.png' },
+  { type: 'bar', img: '/images/charts/bar-heap-on.png' },
+  { type: 'line', img: '/images/charts/line-on.png' },
+  { type: 'area', img: '/images/charts/line-area-on.png' },
+  { type: 'pie', img: '/images/charts/pie-on.png' },
 ];
 
 type IProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
 
 const PanelCharts = ({ chartType, chooseChart, ...others }: IProps) => (
   <Panel {...others} header="图表" key="charts">
-    {charts.map(({ type, img, name }) => (
+    {charts.map(({ type, img }) => (
       <div
         key={type}
         className={classnames({ 'bi-drawer-charts': true, active: type === chartType })}
         onClick={() => chooseChart(type)}
       >
-        <Tooltip placement="bottom" title={name}>
+        <Tooltip placement="bottom" title={chartNameMap[type]}>
           <img src={img} />
         </Tooltip>
       </div>
--- app/router/dashboard/components/chart-drawer/panel-data/index.tsx
+++ app/router/dashboard/components/chart-drawer/panel-data/index.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Collapse } from 'antd';
+import { Collapse, Popover } from 'antd';
+import { chartNameMap } from 'dashboard/utils';
+import './index.scss';
 
 const { Panel } = Collapse;
 
 type IProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
 
-const PanelData = ({ visible, chooseChart, ...others }: IProps) => (
-  <Panel {...others} header="数据" key="data" />
+const DemoData = ({ chartType }: any) => (
+  <div>
+    <p>{chartType}</p>
+  </div>
 );
 
-const mapStateToProps = ({ biDrawer: { visible } }: any) => ({
-  visible,
+const PanelData = ({ chartType, chooseChart, ...others }: IProps) => (
+  <Panel {...others} header="数据" key="data">
+    {chartType && (
+      <Popover
+        placement="left"
+        title={chartNameMap[chartType]}
+        content={<DemoData chartType={chartType} />}
+      >
+        <div className="bi-demo-text">数据示例</div>
+      </Popover>
+    )}
+  </Panel>
+);
+
+const mapStateToProps = ({ biDrawer: { chartType } }: any) => ({
+  chartType,
 });
 
 const mapDispatchToProps = (dispatch: any) => ({

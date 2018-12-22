7ea8b4e6e99429524ae7c82680fead88f3510055
谭立清 <liqing.tlq@alibaba-inc.com>
2018-12-22 20:50:05 +0800

    refactor(panel-data): delete popover

--- app/router/dashboard/components/chart-drawer/panel-data/index.tsx
+++ app/router/dashboard/components/chart-drawer/panel-data/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Collapse, Popover } from 'antd';
+import { Collapse } from 'antd';
 import { chartNameMap } from 'dashboard/utils';
 import './index.scss';
 
@@ -8,22 +8,10 @@ const { Panel } = Collapse;
 
 type IProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
 
-const DemoData = ({ chartType }: any) => (
-  <div>
-    <p>{chartType}</p>
-  </div>
-);
-
 const PanelData = ({ chartType, chooseChart, ...others }: IProps) => (
   <Panel {...others} header="数据" key="data">
     {chartType && (
-      <Popover
-        placement="left"
-        title={chartNameMap[chartType]}
-        content={<DemoData chartType={chartType} />}
-      >
-        <div className="bi-demo-text">数据示例</div>
-      </Popover>
+      <div className="bi-demo-text">{`${chartNameMap[chartType]}数据示例复制`}</div>
     )}
   </Panel>
 );

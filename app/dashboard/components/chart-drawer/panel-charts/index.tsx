4a4b366e3abf5e9923c2d1a961983ba94568941b
谭立清 <liqing.tlq@alibaba-inc.com>
2018-12-26 11:50:06 +0800

    refactor: change image to icon

--- app/dashboard/components/chart-drawer/panel-charts/index.tsx
+++ app/dashboard/components/chart-drawer/panel-charts/index.tsx
@@ -2,31 +2,31 @@ import React from 'react';
 import { get } from 'lodash';
 import classnames from 'classnames';
 import { connect } from 'dva';
-import { Collapse, Tooltip } from 'antd';
+import { Collapse, Tooltip, Icon } from 'antd';
 import { chartNameMap } from 'dashboard/utils';
 import './index.scss';
 
 const { Panel } = Collapse;
 
 const charts = [
-  { type: 'bar', img: '/images/charts/bar-heap-on.png' },
-  { type: 'line', img: '/images/charts/line-on.png' },
-  { type: 'area', img: '/images/charts/line-area-on.png' },
-  { type: 'pie', img: '/images/charts/pie-on.png' },
+  { type: 'bar' }, // img: '/images/charts/bar-heap-on.png'
+  { type: 'line' }, // img: '/images/charts/line-on.png'
+  { type: 'area' }, // img: '/images/charts/line-area-on.png'
+  { type: 'pie' }, // img: '/images/charts/pie-on.png'
 ];
 
 type IProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
 
 const PanelCharts = ({ chartType, chooseChart, ...others }: IProps) => (
   <Panel {...others} header="图表" key="charts">
-    {charts.map(({ type, img }) => (
+    {charts.map(({ type }) => (
       <div
         key={type}
         className={classnames({ 'bi-drawer-charts': true, active: type === chartType })}
         onClick={() => chooseChart(type)}
       >
         <Tooltip placement="bottom" title={chartNameMap[type]}>
-          <img src={img} />
+          <Icon type={`${type}-chart`} />
         </Tooltip>
       </div>
     ))}

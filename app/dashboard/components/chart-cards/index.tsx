35434f83ee792ae263fbe620b9a7da2f3760ad27
陈韡韬 <cwt194536@alibaba-inc.com>
2018-12-28 15:59:35 +0800

    feat: add cards

--- app/dashboard/components/chart-cards/index.tsx
+++ app/dashboard/components/chart-cards/index.tsx
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import { connect } from 'dva';
+import { get } from 'lodash';
 import { Icon } from 'antd';
 import { ReactEchartsPropsTypes } from '../../types';
 import { mockDataCards } from './utils';
@@ -20,15 +21,28 @@ interface IProps extends ReturnType<typeof mapStateToProps>, ReactEchartsPropsTy
   isMock?: boolean
 }
 
-const ChartCards = ({ option = {}, names = [], datas, isMock }: IProps) => {
-  const realDatas: any[] = isMock ? mockDataCards.datas : (datas || []);
-  const { layout } = isMock ? mockDataCards.option : option;
-  const realNames: any[] = isMock ? mockDataCards.names : (names || []);
-  const dataSource: any[] = realNames.map((name, i) => ({ name, data: realDatas[0].data[i] }));
-  if (layout.fieldsCount !== dataSource.length) {
+const convertProportion = (proportionInput: []) => {
+  let fieldsCount: number = 0;
+  const config = proportionInput.map((proportion: any, i) => {
+    fieldsCount += proportion.cols;
+    return { rowNo: i + 1, ...proportion };
+  });
+  return { fieldsCount, config };
+};
+
+const ChartCards = ({ option = {}, isMock, ...others }: IProps) => {
+  const names = get(others, ['names'], []);
+  const datas = get(others, ['datas'], []);
+  const realNames: any = isMock ? mockDataCards.names : names;
+  const realDatas: any = isMock ? mockDataCards.datas : datas;
+  const { proportion = [] } = isMock ? mockDataCards.option : option;
+  const layoutSource: any[] = realNames.map((name: string, i: number) => ({ name, data: realDatas[0].data[i] }));
+  const cardsProportion = convertProportion(proportion);
+  if (cardsProportion.fieldsCount !== layoutSource.length) {
     console.error('fields count not match');
-    return;
+    return null;
   }
+
   return (
     <React.Fragment>
       {isMock && (
@@ -37,21 +51,21 @@ const ChartCards = ({ option = {}, names = [], datas, isMock }: IProps) => {
         <div className="bi-mask-text">模拟数据展示</div>
       </div>
       )}
-      <section className="cards-layout">
+      <section className="bi-cards-layout">
         {
-          layout.config.map((rowConfig: any) => {
-            const { cols, proportion } = rowConfig;
-            const source = dataSource.splice(0, cols);
+          cardsProportion.config.map((rowConfig: any) => {
+            const { cols, scale } = rowConfig;
+            const source = layoutSource.splice(0, cols);
             return (
-              <div className="cards-row" key={rowConfig.rowNo}>
+              <div className="bi-cards-row" key={rowConfig.rowNo}>
                 {
                     source.map((data, i) => {
                       const { name, data: { value, status } } = data;
-                      const flex: number = proportion ? proportion[i] : 1;
+                      const flex: number = scale ? scale[i] : 1;
                       return (
-                        <div key={name} className="cards-block" style={{ flex }}>
-                          <div className="cards-block-title"><Icon type="bar-chart" /><span>{name}</span></div>
-                          <div className="cards-block-content"><span>{value}</span>{status !== 'none' && <Icon type={status} />}</div>
+                        <div key={name} className="bi-cards-block" style={{ flex }}>
+                          <div className="bi-cards-block-title"><Icon type="bar-chart" /><span>{name}</span></div>
+                          <div className="bi-cards-block-content"><span>{value}</span>{status !== 'none' && <Icon type={status} />}</div>
                         </div>);
                     })
                 }

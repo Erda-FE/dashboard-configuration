f1a462d1071a7b3e400eea4b6e4d0f00c7753343
谭立清 <liqing.tlq@alibaba-inc.com>
2019-01-03 21:51:27 +0800

    feat(select-normal): add UrlComponent

--- src/components/controls/select-normal/data-settings/index.tsx
+++ src/components/controls/select-normal/data-settings/index.tsx
@@ -1,38 +1,44 @@
 import React from 'react';
 import { Form, Input } from 'antd';
 import { FormComponentProps } from 'antd/lib/form';
+import PropTypes from 'prop-types';
 import { formItemLayout, pannelControlPrefix } from '../../../utils';
 
 const { TextArea } = Input;
 
-const DataSettings = ({ form }: FormComponentProps) => {
-  const { getFieldDecorator } = form;
-  return (
-    <React.Fragment>
-      <Form.Item label="参数名称" {...formItemLayout}>
-        {getFieldDecorator(`${pannelControlPrefix}searchName`, {
-          rules: [{
-            required: true,
-            message: '请输入在接口中的查询名称',
-          }],
-        })(<Input placeholder="请输入在接口中的查询名称，一般为英文，示例：id"/>)}
-      </Form.Item>
-      <Form.Item label="控件接口" {...formItemLayout}>
-        {getFieldDecorator(`${pannelControlPrefix}url`, {
-          rules: [{
-            message: '请输入控件接口',
-          }],
-        })(<Input placeholder="请输入控件接口，用于动态获取控件数据"/>)}
-      </Form.Item>
-      <Form.Item label="固定数据" {...formItemLayout}>
-        {getFieldDecorator(`${pannelControlPrefix}fixedData`, {
-          rules: [{
-            message: '请输入固定数据',
-          }],
-        })(<TextArea placeholder={`${''}请输入标准JSON格式的固定数据，用于静态的控件数据，示例：[{"name": 'lucy', "value": 1}]`} />)}
-      </Form.Item>
-    </React.Fragment>
-  );
-};
+export default class DataSettings extends React.PureComponent<FormComponentProps> {
+  static contextTypes = {
+    UrlComponent: PropTypes.func
+  };
 
-export default DataSettings;
+  render() {
+    const UrlComponent = this.context.UrlComponent;
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <React.Fragment>
+        <Form.Item label="参数名称" {...formItemLayout}>
+          {getFieldDecorator(`${pannelControlPrefix}searchName`, {
+            rules: [{
+              required: true,
+              message: '请输入在接口中的查询名称',
+            }],
+          })(<Input placeholder="请输入在接口中的查询名称，一般为英文，示例：id"/>)}
+        </Form.Item>
+        <Form.Item label="控件接口" {...formItemLayout}>
+          {getFieldDecorator(`${pannelControlPrefix}url`, {
+            rules: [{
+              message: '请输入控件接口',
+            }],
+          })(<UrlComponent placeholder="请输入控件接口，用于动态获取控件数据"/>)}
+        </Form.Item>
+        <Form.Item label="固定数据" {...formItemLayout}>
+          {getFieldDecorator(`${pannelControlPrefix}fixedData`, {
+            rules: [{
+              message: '请输入固定数据',
+            }],
+          })(<TextArea placeholder={`${''}请输入标准JSON格式的固定数据，用于静态的控件数据，示例：[{"name": 'lucy', "value": 1}]`} />)}
+        </Form.Item>
+      </React.Fragment>
+    );
+  }
+}

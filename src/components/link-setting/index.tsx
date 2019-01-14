67a4485cb5f9b313333018448f4fd814e655185d
谭立清 <liqing.tlq@alibaba-inc.com>
2019-01-14 15:02:13 +0800

    feat(chart): add name to input

--- src/components/link-setting/index.tsx
+++ src/components/link-setting/index.tsx
@@ -2,33 +2,56 @@
  * 联动设置
  */
 import React from 'react';
+import { filter, isEmpty, get } from 'lodash';
 import { connect } from 'dva';
-import { Modal, Form } from 'antd';
+import { Modal, Form, Input } from 'antd';
 import { FormComponentProps } from 'antd/lib/form';
 
-type IProps = FormComponentProps & ReturnType<typeof mapStateToProps>;
+type IProps = FormComponentProps & ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
+
+export const formItemLayout = {
+  labelCol: {
+    span: 12,
+  },
+  wrapperCol: {
+    span: 12,
+  },
+};
 
 class LinkSettingModal extends React.PureComponent<IProps> {
   onOk = () => {
-
-  }
-
-  onCancel = () => {
-
+    const { form: { validateFields } } = this.props;
+    validateFields((err: any, values) => {
+      if (err) return;
+      console.log('values', values);
+    });
   }
 
   render() {
-    const { linkId } = this.props;
+    const { linkId, closeLinkSetting, drawerInfoMap, layout, form: { getFieldDecorator } } = this.props;
+    const otherCharts = filter(layout, ({ i }) => i !== linkId);
     return (
       <Modal
         title="联动设置"
         visible={!!linkId}
         onOk={this.onOk}
-        onCancel={this.onCancel}
+        onCancel={closeLinkSetting}
         okText="保存"
         cancelText="取消"
+        maskClosable={false}
       >
-      联动设置xxx
+        {isEmpty(otherCharts) ? '无可联动图表' : otherCharts.map(({ i }: any) => {
+          const key = i as string;
+          return (
+            <Form.Item key={key} label={get(drawerInfoMap, [key, 'name'], key)} {...formItemLayout}>
+              {getFieldDecorator(key, {
+                rules: [{
+                  message: '请输入联动参数名称，一般为英文',
+                }],
+              })(<Input placeholder="请输入联动参数名称，一般为英文" />)}
+            </Form.Item>
+          );
+        })}
       </Modal>
     );
   }
@@ -36,8 +59,18 @@ class LinkSettingModal extends React.PureComponent<IProps> {
 
 const mapStateToProps = ({
   linkSetting: { linkId },
+  biDashBoard: { layout },
+  biDrawer: { drawerInfoMap },
 }: any) => ({
   linkId,
+  layout,
+  drawerInfoMap,
 });
 
-export default connect(mapStateToProps)(Form.create()(LinkSettingModal));
+const mapDispatchToProps = (dispatch: any) => ({
+  closeLinkSetting() {
+    dispatch({ type: 'linkSetting/closeLinkSetting' });
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form.create()(LinkSettingModal));

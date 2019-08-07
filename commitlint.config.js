2531e7b118a6c692926a00d489ca41e6dab96285
陈韡韬 <cwt194536@alibaba-inc.com>
2019-08-07 16:56:22 +0800

    feat_skip: add git cz

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,31 @@
+const read = require('@commitlint/read');
+
+const suffixTypeMap = {
+  skip: '_skip',
+};
+
+const prefixList = ['feat', 'fix', 'refactor'];
+
+const getTypeEnum = () => prefixList.map(prefix => `${prefix}${suffixTypeMap.skip}`);
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', [
-      'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert', 'temp',
+      ...getTypeEnum(), ...prefixList,
+      'docs', 'style', 'perf', 'test', 'chore', 'revert', 'WIP',
     ]],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
+    'type-case': [1, 'always', 'snake-case'],
+    'body-empty': async () => {
+      let bodyEmpty = [2, 'always'];
+      await read({ edit: true }).then((message) => {
+        const commitStr = message.toString();
+        const type = commitStr.split(':')[0];
+        if (prefixList.includes(type) && !type.endsWith(suffixTypeMap.skip)) {
+          bodyEmpty = [2, 'never'];
+        }
+      });
+      return bodyEmpty;
+    },
   },
 };

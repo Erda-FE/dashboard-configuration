d2457b58dce914af43ea687ea17c98c6aea4e94b
李操 <lc195631@alibaba-inc.com>
2020-04-23 14:10:55 +0800

    fix_skip: support table col unit

--- src/components/charts/chart-table/index.tsx
+++ src/components/charts/chart-table/index.tsx
@@ -4,24 +4,40 @@
 import React from 'react';
 import { connect } from 'dva';
 import { Table } from 'antd';
+import { map } from 'lodash';
 
 interface IProps extends ReturnType<typeof mapStateToProps> {
   viewId: string;
   results: { [k: string]: any }[];
-  cols: { title: string; dataIndex: string; }[];
+  cols: { title: string; dataIndex: string; unit?: string; render?: any }[];
 }
 
-const ChartTable = ({ results = [], cols }: IProps) => (
-  <React.Fragment>
-    <section className="table-panel">
-      <Table
-        rowKey="id"
-        columns={cols}
-        dataSource={results}
-        pagination={false}
-      />
-    </section>
-  </React.Fragment>);
+const ChartTable = ({ results = [], cols }: IProps) => {
+  const _cols = map(cols, (col) => {
+    let r = {
+      ...col,
+      key: col.dataIndex,
+    };
+    if (col.unit) {
+      r = {
+        ...r,
+        render: (v: any) => `${v}${col.unit}`,
+      };
+    }
+    return r;
+  });
+  return (
+    <React.Fragment>
+      <section className="table-panel">
+        <Table
+          columns={_cols}
+          dataSource={results}
+          pagination={false}
+        />
+      </section>
+    </React.Fragment>
+  );
+};
 
 const mapStateToProps = ({ chartEditor: { viewMap } }: any, { viewId, data: { metricData: results, cols } }: any) => ({
   results,

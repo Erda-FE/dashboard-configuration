c277a686ca3d29da6442a754e6e0939ae6965135
李操 <lc195631@alibaba-inc.com>
2020-06-21 11:11:42 +0800

    fix_skip: configurator style

--- src/editor/data-config/default-api-form.tsx
+++ src/editor/data-config/default-api-form.tsx
@@ -12,7 +12,61 @@ export default ({ submitResult, getCurrentChart, form }: IProps) => {
   const getFieldsList = () => [
     {
       label: 'API 配置',
-      name: 'api',
+      name: 'data.aaa',
+      type: 'textArea',
+      required: true,
+      itemProps: {
+        placeholder: '请输入 API 的 JSON 配置',
+      },
+    },
+    {
+      label: 'API 配置',
+      name: 'data.aaa',
+      type: 'textArea',
+      required: true,
+      itemProps: {
+        placeholder: '请输入 API 的 JSON 配置',
+      },
+    },
+    {
+      label: 'API 配置',
+      name: 'data.aaa',
+      type: 'textArea',
+      required: true,
+      itemProps: {
+        placeholder: '请输入 API 的 JSON 配置',
+      },
+    },
+    {
+      label: 'API 配置',
+      name: 'data.aaa',
+      type: 'textArea',
+      required: true,
+      itemProps: {
+        placeholder: '请输入 API 的 JSON 配置',
+      },
+    },
+    {
+      label: 'API 配置',
+      name: 'data.aaa',
+      type: 'textArea',
+      required: true,
+      itemProps: {
+        placeholder: '请输入 API 的 JSON 配置',
+      },
+    },
+    {
+      label: 'API 配置',
+      name: 'data.aaa',
+      type: 'textArea',
+      required: true,
+      itemProps: {
+        placeholder: '请输入 API 的 JSON 配置',
+      },
+    },
+    {
+      label: 'API 配置',
+      name: 'data.aaa',
       type: 'textArea',
       required: true,
       itemProps: {

2fbe4097b2fe448edf4158024cd7affdd103be86
李操 <lc195631@alibaba-inc.com>
2020-09-04 15:46:44 +0800

    fix_skip: fix style

--- src/components/views/chart-table/index.tsx
+++ src/components/views/chart-table/index.tsx
@@ -11,16 +11,19 @@ interface IProps {
   cols: { title: string; dataIndex: string; unit?: string; render?: any }[];
 }
 
+const fixedLimit = 5;
+const fixedWidth = 150;
 const ChartTable = ({ results = [], cols }: IProps) => {
+  const isOverLimit = cols.length > fixedLimit;
   const _cols = map(cols, (col, index) => {
     let r = {
       ...col,
       key: col.dataIndex,
     };
-    if (index === 0 && cols.length > 5) {
+    if (index === 0 && isOverLimit) {
       r = {
         ...r,
-        width: 150,
+        width: fixedWidth,
         fixed: 'left',
       };
     }
@@ -39,7 +42,7 @@ const ChartTable = ({ results = [], cols }: IProps) => {
           rowKey={'id'}
           columns={_cols}
           dataSource={results}
-          scroll={cols.length > 5 ? { x: 150 + cols.length * 200 } : undefined}
+          scroll={isOverLimit ? { x: fixedWidth + cols.length * 200 } : undefined}
           pagination={{
             hideOnSinglePage: true,
           }}

1c142c1c78bdbdf26e22c887aa29194428980290
daskyrk <daskyrk@163.com>
2021-05-21 16:21:21 +0800

    fix: add api org prefix

--- src/services/chart-editor.ts
+++ src/services/chart-editor.ts
@@ -1,8 +1,20 @@
+import { get } from 'lodash';
 import { DC } from 'src/types';
 import agent from '../common/utils/agent';
 
+
+export const getOrgFromPath = () => {
+  return get(location.pathname.split('/'),'[1]') || '-';
+}
+
+export const setApiWithOrg = (api: string) => {
+  return api.startsWith('/api/')
+    ? api.replace('/api/',`/api/${getOrgFromPath()}/`)
+    : api;
+}
+
 export const getChartData = ({ url, query, method = 'get', body }: DC.API) => (
-  agent[method.toLowerCase()](url)
+  agent[method.toLowerCase()](setApiWithOrg(url))
     .query(query)
     .send(body)
     .then((response: any) => response.body)
@@ -25,7 +37,7 @@ interface IExportChartDataPayload {
 
 // 临时加个导出图表数据的 service，后续要支持配置
 export const exportChartData = (metric: string, query: IExportChartDataQuery, payload: IExportChartDataPayload) => {
-  return agent.post(`/api/metrics/${metric}/export`)
+  return agent.post(setApiWithOrg(`/api/metrics/${metric}/export`))
     .responseType('blob')
     .query(query)
     .send(payload)

3dda32608f7612198151d8683006cb42ad8092d5
sherotree <wytreeshero@163.com>
2021-11-18 15:16:14 +0800

    fix: i18n impact erda-ui error (#46)
    * fix: i18n impact erda-ui error
    
    * refactor: complete type

--- src/utils/locale.ts
+++
@@ -1,4 +0,0 @@
-import zhCN from 'antd/lib/locale/zh_CN';
-import enUS from 'antd/es/locale-provider/en_US';
-
-export const localeMap = { zh: zhCN, en: enUS };
---
+++ src/utils/locale.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import zhCN from 'antd/lib/locale/zh_CN';
+import enUS from 'antd/es/locale-provider/en_US';
+import { ConfigProvider } from 'antd';
+
+export const localeMap = { zh: zhCN, en: enUS };
+
+export const Wrapper = ({ children, locale }: {children: React.ReactElement; locale: string}) => {
+  if (process.env.NODE_ENV === 'production') {
+    return <>{children}</>;
+  } else {
+    return <ConfigProvider locale={localeMap[locale]}> {children}</ConfigProvider>;
+  }
+};

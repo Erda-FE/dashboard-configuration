3a2354753b9e0a49414654369b6cf0a8571c76dd
sherotree <wytreeshero@163.com>
2022-04-13 15:25:58 +0800

    fix: remove require icon

--- src/components/DcCharts/chart-map/configurator.tsx
+++ src/components/DcCharts/chart-map/configurator.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import { CommonConfigurator } from 'src/components/DcCharts/common';
-import ChartEditorStore from 'src/stores/chart-editor';
 
 export default () => {
-  const viewCopy = ChartEditorStore.useStore((s) => s.viewCopy);
-  const { updateEditor } = ChartEditorStore;
-  const fields = [{}];
-
-  return <CommonConfigurator fields={fields} />;
+  return <CommonConfigurator />;
 };
